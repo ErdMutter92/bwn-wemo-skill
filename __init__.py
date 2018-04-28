23e09608fde31b48a4f0c3b41bb2a972db343f52
Brandon Bleau <bmbleau@gmail.com>
2018-04-28 00:34:54 -0400

    Update skill to controll wemo devices
    Adds the ability to control wemo switches via voice commands of
    a toggle word and a switch name. Devices should automatically
    register their names as entities when they come online.

--- __init__.py
+++ __init__.py
@@ -10,45 +10,71 @@
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
+from ouimeaux.environment import Environment
 
 # Each skill is contained within its own class, which inherits base methods
 # from the MycroftSkill class.  You extend this class as shown below.
 
 # TODO: Change "Template" to a unique name for your skill
-class TemplateSkill(MycroftSkill):
+class WemoSkill(MycroftSkill):
 
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
-        super(TemplateSkill, self).__init__(name="TemplateSkill")
-        
-        # Initialize working variables used within the skill.
-        self.count = 0
+        super(WemoSkill, self).__init__(name="WemoSkill")
 
-    # The "handle_xxxx_intent" function is triggered by Mycroft when the
-    # skill's intent is matched.  The intent is defined by the IntentBuilder()
-    # pieces, and is triggered when the user's utterance matches the pattern
-    # defined by the keywords.  In this case, the match occurs when one word
-    # is found from each of the files:
-    #    vocab/en-us/Hello.voc
-    #    vocab/en-us/World.voc
-    # In this example that means it would match on utterances like:
-    #   'Hello world'
-    #   'Howdy you great big world'
-    #   'Greetings planet earth'
-    @intent_handler(IntentBuilder("").require("Hello").require("World"))
-    def handle_hello_world_intent(self, message):
-        # In this case, respond by simply speaking a canned response.
-        # Mycroft will randomly speak one of the lines from the file
-        #    dialogs/en-us/hello.world.dialog
-        self.speak_dialog("hello.world")
+    def initialize(self):
+        self.wemo = Environment(self.on_switch, self.on_motion);
+        self.wemo.start();
+        self.wemo.discover(seconds=3);
 
-    @intent_handler(IntentBuilder("").require("Count").require("Dir"))
-    def handle_count_intent(self, message):
-        if message.data["Dir"] == "up":
-            self.count += 1
-        else:  # assume "down"
-            self.count -= 1
-        self.speak_dialog("count.is.now", data={"count": self.count})
+    def on_switch(self, switch):
+        self.register_vocabulary(switch.name, 'WemoSwitch');
+
+    def on_motion(self, motion):
+        self.register_vocabulary(switch.name, 'WemoMotion');
+
+    def get_toggle_label(self, state):
+        return {
+            0: 'off',
+            1: 'on',
+        }[state]
+
+    @intent_handler(IntentBuilder("").require("Toggle").require('WemoSwitch'))
+    def handle_switch(self, message):
+        name = message.data.get('WemoSwitch')
+        method = message.data.get('Toggle')
+        device = self.wemo.get_switch(name)
+        state = device.get_state()
+        command = getattr(device, method)
+
+        if self.get_toggle_label(state) == method:
+            self.speak_dialog("switch.active", data={"light": device.name, "toggle": method})
+        elif method == 'toggle':
+            self.speak('Toggling ' + device.name)
+            command()
+        else:
+            self.speak_dialog("switch.toggle", data={"light": device.name, "toggle": method})
+            command()
+
+    @intent_handler(IntentBuilder("").require("Find").require("WemoDevices"))
+    def handle_discover(self, message):
+        self.speak('Looking for wemo switches...')
+        self.wemo.start();
+        self.wemo.discover(seconds=5);
+        switches = self.wemo.list_switches();
+        self.speak_dialog('switch.found', data={"count": len(switches)})
+
+    @intent_handler(IntentBuilder("").require("List").require("WemoDevices"))
+    def handle_list_switches(self, message):
+        switches = self.wemo.list_switches();
+        self.speak_dialog("switch.known", data={"count": len(switches)});
+        for index, switch in enumerate(switches, start=1):
+            if (index == len(switches)):
+                self.speak('and ' + switch)
+            else:
+                self.speak(switch)
+
+
 
     # The "stop" method defines what Mycroft does when told to stop during
     # the skill's execution. In this case, since the skill's functionality
@@ -62,4 +88,4 @@ class TemplateSkill(MycroftSkill):
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
 def create_skill():
-    return TemplateSkill()
+    return WemoSkill()

4851dacb8d7bf4789dbeb72668215bc8b98072ab
Brandon Bleau <bmbleau@gmail.com>
2018-05-02 20:15:31 -0400

    Remove todo comments

--- __init__.py
+++ __init__.py
@@ -1,21 +1,8 @@
-# TODO: Add an appropriate license to your skill before publishing.  See
-# the LICENSE file for more information.
-
-# Below is the list of outside modules you'll be using in your skill.
-# They might be built-in to Python, from mycroft-core or from external
-# libraries.  If you use an external library, be sure to include it
-# in the requirements.txt file so the library is installed properly
-# when the skill gets installed later by a user.
-
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
 from ouimeaux.environment import Environment
 
-# Each skill is contained within its own class, which inherits base methods
-# from the MycroftSkill class.  You extend this class as shown below.
-
-# TODO: Change "Template" to a unique name for your skill
 class WemoSkill(MycroftSkill):
 
     # The constructor of the skill, which calls MycroftSkill's constructor
@@ -74,16 +61,13 @@ class WemoSkill(MycroftSkill):
             else:
                 self.speak(switch)
 
-
-
     # The "stop" method defines what Mycroft does when told to stop during
     # the skill's execution. In this case, since the skill's functionality
     # is extremely simple, there is no need to override it.  If you DO
     # need to implement stop, you should return True to indicate you handled
     # it.
-    #
-    # def stop(self):
-    #    return False
+    def stop(self):
+       return False
 
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
